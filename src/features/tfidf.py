d53e48e7a3320fc3f2e993586fe4a7160a4827f8
ardwennem2 <tom_brunner@hotmail.com>
2018-03-16 16:33:33 +0100

    tf-idf is done

--- src/features/tfidf.py
+++ src/features/tfidf.py
@@ -1,35 +1,64 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+import numpy as np
 
-headline = ['queen', 'land', 'stuf', 'queen', 'king', 'sauce', 'a']
-article = ['king', 'king', 'queen', 'land', 'stuf', 'stuf2']
 
-trainNum = 5
-testNum = 0
+with open('data_files/smalldata.pkl', 'rb') as f:
+    data = pickle.load(f, encoding='latin1')
+headline = data['Headline_unigram']
+article = data['articleBody_unigram'] 
 
-vec = TfidfVectorizer(ngram_range=(1, 3), max_df=0.8, min_df=2)
-vec.fit(headline+article) # Tf-idf calculated on the combined training + test set
+trainNum = 100
+testNum = 100
+totalNum = trainNum+testNum
+
+headlineNoVec = headline.map(lambda x: ' '.join(x))
+articleNoVec = article.map(lambda x: ' '.join(x))
+
+text_per_article = []
+for i in range(0, totalNum):
+    text = headlineNoVec[i] + ' ' + articleNoVec[i]
+    text_per_article.append(text)
+
+
+vec = TfidfVectorizer(ngram_range=(1, 3), max_df=.8, min_df=2)
+vec.fit(text_per_article) # Tf-idf calculated on the combined training + test set
 vocabulary = vec.vocabulary_
 
-vecH = TfidfVectorizer(ngram_range=(1, 3), max_df=0.8, min_df=2, vocabulary=vocabulary)
-vecB = TfidfVectorizer(ngram_range=(1, 3), max_df=0.8, min_df=2, vocabulary=vocabulary)
+vecH = TfidfVectorizer(ngram_range=(1, 3), max_df=.8, min_df=2, vocabulary=vocabulary)
+vecA = TfidfVectorizer(ngram_range=(1, 3), max_df=.8, min_df=2, vocabulary=vocabulary)
 
-headlineTfidf = vecH.fit_transform(headline)
-articleTfidf = vecH.fit_transform(article)
+headlineTfidf = vecH.fit_transform(headlineNoVec)
+articleTfidf = vecA.fit_transform(articleNoVec)
 
 headlineTrain = headlineTfidf[:trainNum, :]
 articleTrain = articleTfidf[:trainNum, :]
 if testNum > 0:
     headlineTest = headlineTfidf[trainNum:, :]
     articleTest = articleTfidf[trainNum:, :]
-	
-print(headlineTfidf)
-print()
-print(articleTfidf)
 
-simTfidf = cosine_similarity(headlineTfidf, articleTfidf)
-simTfidfTrain = simTfidf[:trainNum]
-if testNum >0:
-    simTfidfTest = simTfidf[trainNum:]
-	
-print(simTfidf)
+print(headlineTfidf.shape)
+
+
+simVec = []
+for i in range (0, totalNum):
+    simVec.append([cosine_similarity(headlineTfidf[i], articleTfidf[i])[0][0]])
+print(len(simVec))
+
+simTfidfTrain = simVec[:trainNum]
+simTfidfTest = simVec[trainNum:]
+
+with open('feature_pickles/tfidf_head_train.pkl', "wb") as outfile:
+    pickle.dump(headlineTrain, outfile, -1)
+with open('feature_pickles/tfidf_head_test.pkl', "wb") as outfile:
+    pickle.dump(headlineTest, outfile, -1)
+with open('feature_pickles/tfidf_article_train.pkl', "wb") as outfile:
+    pickle.dump(articleTrain, outfile, -1)
+with open('feature_pickles/tfidf_article_test.pkl', "wb") as outfile:
+    pickle.dump(articleTest, outfile, -1)
+with open('feature_pickles/tfidf_sim_train.pkl', "wb") as outfile:
+    pickle.dump(simTfidfTrain, outfile, -1)
+with open('feature_pickles/tfidf_sim_test.pkl', "wb") as outfile:
+    pickle.dump(simTfidfTest, outfile, -1)
+print ('made 6 pickle files: head_train, head_test, article_train, article_test, sim_train, sim_test')
