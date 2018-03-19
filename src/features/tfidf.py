2b84ba6f182a6f53e1607a3bc09862b6d95e82cd
ardwennem2 <tom_brunner@hotmail.com>
2018-03-19 16:22:25 +0100

    added comments

--- src/features/tfidf.py
+++ src/features/tfidf.py
@@ -3,35 +3,41 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 import numpy as np
 
-
+# load data
 with open('data_files/smalldata.pkl', 'rb') as f:
     data = pickle.load(f, encoding='latin1')
 headline = data['Headline_unigram']
 article = data['articleBody_unigram']
 
+#Number of train and test articles
 trainNum = 100
 testNum = 100
 totalNum = trainNum+testNum
 
+#join the individual words into a sentence ({{'I', 'ate', 'pie'},...} becomes {{'I ate pie'},...})
 headlineNoVec = headline.map(lambda x: ' '.join(x))
 articleNoVec = article.map(lambda x: ' '.join(x))
 
+#Combine headlines and articles
 text_per_article = []
 for i in range(0, totalNum):
     text = headlineNoVec[i] + ' ' + articleNoVec[i]
     text_per_article.append(text)
 
-
+#First use a tfidfVectorizer to find all words
 vec = TfidfVectorizer(ngram_range=(1, 3), max_df=.8, min_df=2)
 vec.fit(text_per_article) # Tf-idf calculated on the combined training + test set
 vocabulary = vec.vocabulary_
 
+#Make different vectorizers for headlines and articles, it is necesary to use 2 different vectorizers
 vecH = TfidfVectorizer(ngram_range=(1, 3), max_df=.8, min_df=2, vocabulary=vocabulary)
 vecA = TfidfVectorizer(ngram_range=(1, 3), max_df=.8, min_df=2, vocabulary=vocabulary)
 
+#Find Tfidf values for headlines and articles
 headlineTfidf = vecH.fit_transform(headlineNoVec)
 articleTfidf = vecA.fit_transform(articleNoVec)
 
+#Make seperate variables for taining and testing
 headlineTrain = headlineTfidf[:trainNum, :]
 articleTrain = articleTfidf[:trainNum, :]
 if testNum > 0:
@@ -40,15 +46,17 @@ if testNum > 0:
 
 print(headlineTfidf.shape)
 
-
+#Find cosine similarities
 simVec = []
 for i in range (0, totalNum):
     simVec.append([cosine_similarity(headlineTfidf[i], articleTfidf[i])[0][0]])
 print(len(simVec))
 
+#make seperate variables for tain and test similarites.
 simTfidfTrain = simVec[:trainNum]
 simTfidfTest = simVec[trainNum:]
 
+#store in pickle files
 with open('feature_pickles/tfidf_head_train.pkl', "wb") as outfile:
     pickle.dump(headlineTrain, outfile, -1)
 with open('feature_pickles/tfidf_head_test.pkl', "wb") as outfile:
--- src/features/word2vec.py
+++ src/features/word2vec.py
@@ -4,19 +4,22 @@ from sklearn.preprocessing import normalize
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 
-
+#load model from google
 model = gensim.models.KeyedVectors.load_word2vec_format('data_files/GoogleNews-vectors-negative300.bin', binary=True)
 
+#load data
 with open('data_files/smalldata.pkl', 'rb') as f:
     data = pickle.load(f, encoding='latin1')
 headline = data['Headline_unigram']
 article = data['articleBody_unigram']
 
+#Number of train and test articles
 trainNum = 100
 testNum = 100
 totalNum = trainNum+testNum
 
 
+#For every every headline/article: look up the vectors for every word in it, add them up, and normalize those vectors.
 i = 0
 headlineVec=[]
 for x in headline:
@@ -36,17 +39,17 @@ for x in article:
         if y in model:
             z = z +np.add(model.get_vector(y), [0.]*300)
     articleVec.append(z)
-articleVec = list(headlineVec)
-articleVec = normalize(headlineVec)
+articleVec = list(articleVec)
+articleVec = normalize(articleVec)
 
-
+#make seperate variables for tain and test headlines/articles, currently this is not used.
 headlineTrain = headlineVec[:trainNum, :]
 articleTrain = articleVec[:trainNum, :]
 if testNum > 0:
     headlineTest = headlineVec[trainNum:, :]
     articleTest = articleVec[trainNum:, :]
 
-
+#Calculate cosine-similarity between headlines and respective articles and make variables for train and test similarities.
 simVecTemp = cosine_similarity(headlineVec, articleVec)
 simVec = []
 for i in range(0, totalNum):
@@ -55,6 +58,7 @@ simVecTrain = simVec[:trainNum]
 if testNum > 0:
     simVecTest = simVec[trainNum:]
 
+#Store in pickles
 with open('feature_pickles/word2vec_sim_train.pkl', "wb") as outfile:
     pickle.dump(simVecTrain, outfile, -1)
 with open('feature_pickles/word2vec_sim_test.pkl', "wb") as outfile:
